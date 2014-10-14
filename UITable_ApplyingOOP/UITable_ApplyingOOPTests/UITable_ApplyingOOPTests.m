f28072b350eca427e2f74ba8c9200682fe1a84a7
marvinlab <m.labrador@klab.com>
2014-10-14 16:46:56 +0800

    UITable_ApplyingOOP_EMPTY

---
+++ UITable_ApplyingOOP/UITable_ApplyingOOPTests/UITable_ApplyingOOPTests.m
@@ -0,0 +1,34 @@
+//
+//  UITable_ApplyingOOPTests.m
+//  UITable_ApplyingOOPTests
+//
+//  Created by Marvin Labrador on 10/14/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UITable_ApplyingOOPTests : XCTestCase
+
+@end
+
+@implementation UITable_ApplyingOOPTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
