56eae7411f53cd1007c654765b3dc5ce62c36a37
marvinlab <m.labrador@klab.com>
2014-10-14 16:11:04 +0800

    TextAndWeb_ApplyingCustomFont_EMPTY

---
+++ TextAndWeb_ApplyingCustomFont/TextAndWeb_ApplyingCustomFont/TCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCAppDelegate.h
+//  TextAndWeb_ApplyingCustomFont
+//
+//  Created by Marvin Labrador on 10/14/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TextAndWeb_ApplyingCustomFont/TextAndWeb_ApplyingCustomFontTests/TextAndWeb_ApplyingCustomFontTests.m
@@ -0,0 +1,34 @@
+//
+//  TextAndWeb_ApplyingCustomFontTests.m
+//  TextAndWeb_ApplyingCustomFontTests
+//
+//  Created by Marvin Labrador on 10/14/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TextAndWeb_ApplyingCustomFontTests : XCTestCase
+
+@end
+
+@implementation TextAndWeb_ApplyingCustomFontTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
