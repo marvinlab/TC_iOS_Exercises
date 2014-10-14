9768bf42b47273856d512cd1821d656cd78b5cad
marvinlab <m.labrador@klab.com>
2014-10-14 10:57:41 +0800

    Animations_UIViews_DONE

--- Animations_UIViews_Picnic/Animations_UIViews_Picnic/TCViewController.h
+++ Animations_UIViews_Picnic/Animations_UIViews_Picnic/TCViewController.h
@@ -10,4 +10,5 @@
 
 @interface TCViewController : UIViewController
 
+
 @end
--- Animations_UIViews_Picnic/Animations_UIViews_Picnic/TCViewController.m
+++ Animations_UIViews_Picnic/Animations_UIViews_Picnic/TCViewController.m
@@ -10,6 +10,10 @@
 
 @interface TCViewController ()
 
+@property (nonatomic, weak) IBOutlet UIImageView *basketTop;
+@property (nonatomic, weak) IBOutlet UIImageView *basketBottom;
+
+
 @end
 
 @implementation TCViewController
@@ -26,4 +30,34 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    CGRect basketTopFrame = self.basketTop.frame;
+    basketTopFrame.origin.y = -basketTopFrame.size.height;
+    
+    CGRect basketBottomFrame = self.basketBottom.frame;
+    basketBottomFrame.origin.y = self.view.bounds.size.height;
+    
+//    [UIView beginAnimations:nil context:nil];
+//    [UIView setAnimationDuration:0.5];
+//    [UIView setAnimationDelay:1.0];
+//    [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+//    
+//    self.basketTop.frame = basketTopFrame;
+//    self.basketBottom.frame = basketBottomFrame;
+    
+
+    [UIView animateWithDuration:0.5
+                          delay:1.0
+                        options:UIViewAnimationCurveEaseOut
+                     animations:^{
+                         self.basketTop.frame = basketTopFrame;
+                         self.basketBottom.frame = basketBottomFrame;
+                     }
+                     completion:^(BOOL finished){
+                         NSLog(@"Done!");
+                     }];
+    
+}
 @end
