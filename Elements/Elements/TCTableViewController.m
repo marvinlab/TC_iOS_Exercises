881914d3e40d261fe5a2b09d8f6a72ab607969bc
marvinlab <m.labrador@klab.com>
2014-10-17 19:30:07 +0800

    Elements Project Not yet done

--- Elements/Elements/TCTableViewController.m
+++ Elements/Elements/TCTableViewController.m
@@ -84,29 +84,90 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     NSLog(@"numberOfRowsInSection");
-//    return [self.allViews count];
-    return 1;
+    return [self.allViews count];
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    static NSString *CellIdentifier = @"Cell";
     
-//    cell.textLabel.text = self.allViews[indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
     
-    
-    cell.textLabel.text = @"hi";
+    cell.textLabel.text = self.allViews[indexPath.row];
     
     NSLog(@"%@", self.allViews);
     
     return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *currentSelected = self.allViews[indexPath.row];
+    
+    if ([currentSelected isEqualToString:@"TCButtonViewController"]) {
+        
+        TCButtonViewController *currentView = [[TCButtonViewController alloc]init];
+        [self.navigationController pushViewController:currentView animated:YES];
+        
+    } else if ([currentSelected isEqualToString:@"TCControlViewController"]) {
+        
+        TCControlViewController *currentView = [[TCControlViewController alloc]init];
+        [self.navigationController pushViewController:currentView animated:YES];
+        
+    } else if ([currentSelected isEqualToString:@"TCTextViewController"]) {
+        
+        TCTextViewController *currentView = [[TCTextViewController alloc]init];
+        [self.navigationController pushViewController:currentView animated:YES];
+        
+    } else if ([currentSelected isEqualToString:@"TCPickersViewController"]) {
+        
+        TCPickersViewController *currentView = [[TCPickersViewController alloc]init];
+        [self.navigationController pushViewController:currentView animated:YES];
+        
+    } else if ([currentSelected isEqualToString:@"TCImageViewController"]) {
+        
+        TCImageViewController *currentView = [[TCImageViewController alloc]init];
+        [self.navigationController pushViewController:currentView animated:YES];
+        
+    } else if ([currentSelected isEqualToString:@"TCSegmentedViewController"]) {
+        
+        TCSegmentedViewController *currentView = [[TCSegmentedViewController alloc]init];
+        [self.navigationController pushViewController:currentView animated:YES];
+        
+    } else if ([currentSelected isEqualToString:@"TCToolbarViewController"]) {
+        
+        TCToolbarViewController *currentView = [[TCToolbarViewController alloc]init];
+        [self.navigationController pushViewController:currentView animated:YES];
+        
+    } else if ([currentSelected isEqualToString:@"TCTabBarController"]) {
+        
+        TCTabBarController *currentView = [[TCTabBarController alloc]init];
+        [self.navigationController pushViewController:currentView animated:YES];
+        
+    } else if ([currentSelected isEqualToString:@"TCAlertViewController"]) {
+        
+        TCAlertViewController *currentView = [[TCAlertViewController alloc]init];
+        [self.navigationController pushViewController:currentView animated:YES];
+        
+    } else if ([currentSelected isEqualToString:@"TCActionSheetViewController"]) {
+        
+        TCActionSheetViewController *currentView = [[TCActionSheetViewController alloc]init];
+        [self.navigationController pushViewController:currentView animated:YES];
+        
+    }
+
+    
+    
 }
 
 
