ce6bbef5a22de7963633322c03e7b7eac01c5287
marvinlab <m.labrador@klab.com>
2014-10-20 09:51:52 +0800

    dev_1.0_MVCPattern created levelViewController

---
+++ MVCPattern/MVCPattern/TCLevelViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCLevelViewController.h
+//  MVCPattern
+//
+//  Created by Marvin Labrador on 10/20/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCLevelViewController : UITableViewController
+
+@end
--- MVCPattern/MVCPattern/TCMainTableViewController.m
+++ MVCPattern/MVCPattern/TCMainTableViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "TCMainTableViewController.h"
+#import "TCLevelViewController.h"
 
 @interface TCMainTableViewController () <UITabBarControllerDelegate, UITableViewDataSource>
 
 @property (nonatomic,strong) NSMutableArray *arrayOfCategories;
+@property (nonatomic, retain) NSMutableArray *arrayOfThumbnails;
 
 
 @end
@@ -30,6 +32,8 @@
     NSDictionary *colorDict = [[NSDictionary alloc]initWithObjectsAndKeys:@"Colors",@"Title", nil];
     NSDictionary *foodDict = [[NSDictionary alloc]initWithObjectsAndKeys:@"Foods",@"Title", nil];
     
+    self.arrayOfThumbnails = [[NSMutableArray alloc]initWithObjects:@"letterscat",@"colorscat",@"foodcat", nil];
+    
     
     //Add these Dictionary of Categories as a subview of the TableView
     
@@ -37,13 +41,8 @@
     [self.arrayOfCategories addObject:colorDict];
     [self.arrayOfCategories addObject:foodDict];
     
-    
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.title = @"Main Menu";
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -57,7 +56,7 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -77,63 +76,21 @@
     
     cell.textLabel.text = [NSString stringWithFormat:@"%@", [categories objectForKey:@"Title"]];
     
+    
+    UIImage *picture = [UIImage imageNamed:self.arrayOfThumbnails[indexPath.row]];
+    cell.imageView.image = picture;
+    
     return cell;
 }
 
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Table view delegate
-
-// In a xib-based application, navigation from a table can be handled in -tableView:didSelectRowAtIndexPath:
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Navigation logic may go here, for example:
-    // Create the next view controller.
-    <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:<#@"Nib name"#> bundle:nil];
-    
-    // Pass the selected object to the new view controller.
-    
-    // Push the view controller.
-    [self.navigationController pushViewController:detailViewController animated:YES];
+    TCLevelViewController *levelView = [[TCLevelViewController alloc]init];
+    [self.navigationController pushViewController:levelView animated:YES];
+
 }
-*/
+
+
 
 @end
